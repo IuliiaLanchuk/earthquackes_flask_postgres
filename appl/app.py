8cd00ac4ee20e911a1af4011cf11518a88749866
IuliiaLanchuk <lanchuk.iuliia@gmail.com>
2023-02-02 15:45:35 +0100

    add User in models.py

--- appl/app.py
+++ appl/app.py
@@ -1,6 +1,12 @@
 from appl import create_app
+from appl.models import db
+
+from flask_migrate import Migrate
 
 app = create_app()
+db.init_app(app)
+migrate = Migrate(app, db)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ appl/models.py
@@ -0,0 +1,19 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+class Saver:
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+
+class User(db.Model, Saver):
+    id = db.Column(db.Integer, primary_key = True)
+    nickname = db.Column(db.String(64), unique = True)
+    email = db.Column(db.String(120), unique = True)
+    city = db.Column(db.String(51), db.ForeignKey('location.city'))
+
+    def __repr__(self):
+        return '<User %r> from <%r>' % (self.nickname, self.city)
